fbee3d4205f017120b3ae006ba6f5162417928ab
Роман <roma.korotaev2015@mail.ru>
2024-01-24 21:01:12 +0300

    Change vite config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true,
-    port: 8000,
-    watch: {
-      usePolling: true
-    }
-  }
+  base:"/Test-Project-In-Pro-Scene"
 })
